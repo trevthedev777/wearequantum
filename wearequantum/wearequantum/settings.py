8c9a1ebf39f158b46879dc51189171f82dd01d26
Your Name <bloodsportrecords@gmail.com>
2022-12-13 21:24:57 +0000

    install bootstrap files

--- wearequantum/wearequantum/settings.py
+++ wearequantum/wearequantum/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+CLOUDINARY = {
+      'cloud_name': 'dkqn7olnn',  
+      'api_key': '613318143517633',  
+      'api_secret': 'AUocR1Ud10-it_ldNpOrUZfRGyo',
+}
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
