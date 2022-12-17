0bc12ef35205a4a414da0b261d4aa82e7bc954f6
Your Name <bloodsportrecords@gmail.com>
2022-12-17 17:09:16 +0000

    Create function to validate ageGate credentials

--- wearequantum/static/js/script.js
+++ wearequantum/static/js/script.js
@@ -1,8 +1,32 @@
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
+const form = document.querySelector('form')
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 };
+
+// Functions
+
+function onlyNumbers(e) {
+  const ageGate = document.querySelectorAll('.ageGate');
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const errMsgDiv = document.getElementById('errorMsg');
+
+  let containsNumber = false
+  ageGate.forEach(input => {
+    if(numbers.some(num => input.value.includes(num))) containsNumber = true;
+  })
+  if (containsNumber || ageGate.value !== numbers && ageGate.value == '' || ageGate[2].value < 2004) {
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = 'Invalid Credentials';
+    errMsgDiv.append(paragraph);
+    e.preventDefault()
+  }
+
+};
+
+// Event Listeners
+form.addEventListener('submit', onlyNumbers)
