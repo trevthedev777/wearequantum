86790956e90291902e7db07c6d2a9e78e30458df
Your Name <bloodsportrecords@gmail.com>
2022-12-20 18:54:26 +0000

    Update Function to validate age and close modal

--- wearequantum/static/js/script.js
+++ wearequantum/static/js/script.js
@@ -1,37 +1,48 @@
-const modal = document.getElementById("myModal");
+const modal = document.getElementById("modal");
 const form = document.querySelector('form');
 const loading = document.getElementById("loading");
 const btnSubmit = document.getElementById('btnSubmit');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
 // Functions
-
 function onlyNumbers(e) {
+  e.preventDefault()
   const ageGate = document.querySelectorAll('.ageGate');
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const errMsgDiv = document.getElementById('errorMsg');
 
-  let containsNumber = false
-  ageGate.forEach(input => {
-    if(numbers.some(num => input.value.includes(num))) containsNumber = true;
-  })
-  if (containsNumber || ageGate.value !== numbers && ageGate.value == '' || ageGate[2].value < 2004) {
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = 'Invalid Credentials';
-    errMsgDiv.append(paragraph);
-    e.preventDefault()
-    }  else if (containsNumber = true) {
-        btnSubmit.remove()
-        loading.classList.remove('hide')
+  const year = ageGate[2].value;
+  const month = ageGate[1].value;
+  const day = ageGate[0].value;
+  const userBirthday = new Date(year, month - 1, day);
+
+  // Date Right Now
+  const today = new Date();
+  // Users age in years
+  const acceptingYear = today.getFullYear() - userBirthday.getFullYear()
+
+  if (acceptingYear < 18 || 
+      acceptingYear == 18 &&
+      today.getMonth() < userBirthday.getMonth() ||
+      today.getMonth() == userBirthday.getMonth() &&
+      today.getDate() < userBirthday.getDate()
+      ) {
+      let paragraph = document.createElement('p');
+      paragraph.innerHTML = 'Invalid Credentials';
+      errMsgDiv.append(paragraph);
+  } else {
+    btnSubmit.remove()
+    loading.classList.remove('hide')
+    modal.classList.add('close');
     }
     
 };
+
 
 
 // Event Listeners
